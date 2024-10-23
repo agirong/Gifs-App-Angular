016f44c0db8c87a3b5c484a0142546a7c12d02d8
agirong <aogarrido.g@gmail.com>
2024-10-23 11:04:19 -0600

    HttpClient y consumo de Giphy ApiKey.

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,14 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  private apiKey:string = 'HQnY2DbLWcLaACpMSXGLd08qdFq0eRJp';
+  private apiUrl:string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory(){
     return [...this._tagsHistory]
@@ -20,9 +23,18 @@ export class GifsService {
 
   searchTag(tag: string):void{
     if(tag.length == 0) return;
-
     this.organizeTags(tag);
 
-    console.log(this._tagsHistory);
+    const params = new HttpParams()
+      .set('api_key',this.apiKey)
+      .set('limit','10')
+      .set('q',tag)
+
+    this.http.get(`${this.apiUrl}/search`,{params})
+    .subscribe(resp=>{
+      console.log(resp);
+    })
   }
 }
+
+
